fdd57ff0cb48126843593a2422d8d27b4c64c3d7
Yojhan Cardona <ycardona@merqueo.com>
2023-06-14 21:43:25 -0500

    feat; Se corrigen rutas y se relizan vistas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar bg-primary navbar-dark" data-bs-theme="dark">
+  <nav class="navbar bg-dark navbar-dark" data-bs-theme="dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Students</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -9,10 +9,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link to="/" class="nav-link active" aria-current="page" href="#">List</router-link>
+            <router-link to="/" class="nav-link" aria-current="page" href="#">List</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/create" class="nav-link" href="#">Create</router-link>
+            <router-link to="/create" class="nav-link" href="#">Register</router-link>
           </li>
         </ul>
       </div>
@@ -22,4 +22,3 @@
   <router-view />
 </template>
 
-<style></style>
